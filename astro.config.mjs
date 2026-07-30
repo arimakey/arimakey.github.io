383837afa0f2385311049fcd85d76297deaf2b97
ar1m4k3y <121048453+arimakey@users.noreply.github.com>
2026-07-29 19:18:46 -0500

    feat: add project grid, dynamic routing, and SPA-like navigation with base URL update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arimakey.github.io',
-  base: 'open-portfolio',
+  base: '/',
   trailingSlash: 'never',
   output: 'static',
   integrations: [mdx(), sitemap(), icon()],
